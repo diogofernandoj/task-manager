19602c8481ff504f1ab507ec80c64500c540402e
Diogo Jorge <diogofernandoj@gmail.com>
2024-07-12 10:24:19 -0300

    feat: add Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,26 @@
+import { ReactNode } from 'react'
+
+interface ButtonProps {
+  children: ReactNode
+  variant?: 'primary' | 'ghost'
+}
+
+const Button = ({ children, variant = 'primary' }: ButtonProps) => {
+  const getVariantClasses = () => {
+    if (variant === 'primary') {
+      return 'bg-[#00A4B5] text-white'
+    } else if (variant === 'ghost') {
+      return 'bg-transparent text-[#818181]'
+    }
+  }
+
+  return (
+    <button
+      className={`py-1 px-2 rounded-md text-xs hover:opacity-75 transition ${getVariantClasses()}`}
+    >
+      {children}
+    </button>
+  )
+}
+
+export default Button
