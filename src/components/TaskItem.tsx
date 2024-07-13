b1e40e4118cb63ae79ed31ee3a4bf6662ce35f8f
Diogo Jorge <diogofernandoj@gmail.com>
2024-07-13 12:06:42 -0300

    feat: ensure TaskItem checkbox changes depending on task status

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -1,4 +1,5 @@
 import { ITask } from './Tasks'
+import { FaCheck, FaSpinner } from 'react-icons/fa'
 
 interface TaskItemProps {
   task: ITask
@@ -7,11 +8,11 @@ interface TaskItemProps {
 const TaskItem = ({ task }: TaskItemProps) => {
   const getStatusClasses = () => {
     if (task.status === 'done') {
-      return 'bg-[#00ADB5] bg-opacity-10 text-[#00ADB5]'
+      return 'bg-[#00ADB5] text-[#00ADB5]'
     }
 
     if (task.status === 'doing') {
-      return 'bg-[#FFAA04] bg-opacity-10 text-[#FFAA04]'
+      return 'bg-[#FFAA04] text-[#FFAA04]'
     }
 
     if (task.status === 'todo') {
@@ -21,9 +22,23 @@ const TaskItem = ({ task }: TaskItemProps) => {
 
   return (
     <div
-      className={`flex px-4 py-3 text-sm items-center gap-2 ${getStatusClasses()}`}
+      className={`bg-opacity-10 flex px-4 py-3 text-sm items-center gap-2 ${getStatusClasses()}`}
     >
-      <input type="checkbox" />
+      <label
+        htmlFor={task.title}
+        className={`h-5 w-5 rounded-sm flex items-center justify-center ${getStatusClasses()}`}
+      >
+        <input
+          type="checkbox"
+          checked={task.status === 'done'}
+          className="hidden"
+          id={task.title}
+        />
+        {task.status === 'done' && <FaCheck className="text-white" size={12} />}
+        {task.status === 'doing' && (
+          <FaSpinner className="text-white animate-spin" size={12} />
+        )}
+      </label>
       <p>{task.title}</p>
     </div>
   )
