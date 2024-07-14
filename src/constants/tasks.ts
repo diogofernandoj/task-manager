92d9220370681037e4592b42ae32283e14260075
Diogo Jorge <diogofernandoj@gmail.com>
2024-07-14 11:12:15 -0300

    feat: add constant task in a separated file

---
+++ src/constants/tasks.ts
@@ -0,0 +1,32 @@
+import { ITask } from '../components/Tasks'
+
+export const TASKS: ITask[] = [
+  {
+    id: 1,
+    title: 'Estudar',
+    description: 'lorem ipsum',
+    time: 'morning',
+    status: 'done',
+  },
+  {
+    id: 2,
+    title: 'Ler',
+    description: 'lorem ipsum',
+    time: 'morning',
+    status: 'doing',
+  },
+  {
+    id: 3,
+    title: 'Trabalhar',
+    description: 'lorem ipsum',
+    time: 'afternoon',
+    status: 'todo',
+  },
+  {
+    id: 4,
+    title: 'Jantar',
+    description: 'lorem ipsum',
+    time: 'evening',
+    status: 'todo',
+  },
+]
