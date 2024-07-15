ff502f0a4dfeb83b6711e42cfa89e27f77bf9151
Diogo Jorge <diogofernandoj@gmail.com>
2024-07-15 11:49:24 -0300

    feat: add secondary variant to button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,22 +2,43 @@ import { ButtonHTMLAttributes, ReactNode } from 'react'
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode
-  variant?: 'primary' | 'ghost'
+  variant?: 'primary' | 'secondary' | 'ghost'
+  size?: 'small' | 'large'
 }
 
-const Button = ({ children, variant = 'primary', ...rest }: ButtonProps) => {
+const Button = ({
+  children,
+  variant = 'primary',
+  size = 'small',
+  className,
+  ...rest
+}: ButtonProps) => {
   const getVariantClasses = () => {
     if (variant === 'primary') {
       return 'bg-[#00A4B5] text-white'
-    } else if (variant === 'ghost') {
+    }
+    if (variant === 'secondary') {
+      return 'bg-[#EEEEEE] text-gray-800'
+    }
+    if (variant === 'ghost') {
       return 'bg-transparent text-[#818181]'
+    }
+  }
+
+  const getSizeClasses = () => {
+    if (size === 'small') {
+      return 'py-1 text-xs'
+    }
+
+    if (size === 'large') {
+      return 'py-2 text-sm'
     }
   }
 
   return (
     <button
       {...rest}
-      className={`flex items-center gap-1 font-semibold py-1 px-2 rounded-md text-xs hover:opacity-75 transition ${getVariantClasses()}`}
+      className={`${className} flex justify-center items-center gap-1 font-semibold px-2 rounded-md hover:opacity-75 transition ${getVariantClasses()} ${getSizeClasses()}`}
     >
       {children}
     </button>
